b42aee7fb26f1dec59b9217724918c2f2a8e4dfd
tamzheartbeatz <tammy@flyfreebird.com>
2013-07-02 09:27:47 -0700

    add new core files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Contact::Application.config.secret_token = '7a8f4beac02faee4a27339265e3ab31b91db8c8441a5076962b8c2db74dc845ed104b1c314c6fa7d8f208e570ff17fd116994dacbd5c09578cc4ee3d002ee78a'
